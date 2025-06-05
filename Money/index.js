3b429938db330f0ff30a36aedbb61ef916ece02e
Sharma0807 <bhartisharma25@navgurukul.org>
2025-06-05 10:26:01 +0530

    first commit

---
+++ Money/index.js
@@ -0,0 +1,68 @@
+// let a=3;
+// if(a%2==0){
+//     console.log("even")
+// } else{
+//     console.log("odd")
+// }
+
+
+
+// let  i=0;
+// let  n=10;
+// while(i<=n) {
+//     console.log(i);
+//     i++;
+// }
+
+// 
+// let i=1;
+// let n=10;
+// while(i<=n){
+//     if(i%2!=0);{
+//     console.log(i);
+// } else{
+//      ( n%11==0);
+//      console.log
+// }
+// i++;
+// }
+
+
+
+
+
+// let a=7;
+// let b=9;
+// sum =a+b;
+// if(sum>=15 && sum<=20){
+//     console.log(20)
+// }
+
+
+// let i=1;
+// num=5;
+// sum=0
+// for(leti=0; i<=5; i++){
+//     sum=sum+i
+//     console.log(sum)
+// }
+
+
+
+// 
+
+// let n=122;
+// let sum=0;
+// while(n>0 ){
+//      let R=n%10
+//      sum=sum+R
+//      n = Math.floor(n/10)
+// }
+
+// console.log(sum)
+
+
+let i = 10; 
+  if(i >= 1);  {
+  console.log(i);
+}
